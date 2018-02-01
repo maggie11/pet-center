6c8566ecf43697c3a22589432847c02647053e6a
沈妍 <1016294791@qq.com>
2018-02-01 17:32:17 +0800

    save

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 /**
- * 宠物中心
+ * 宠物中心启动文件
  */
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
